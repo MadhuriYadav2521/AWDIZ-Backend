0cc13066b624ca7577bdd6ab75d9d66ac046a00b
MadhuriYadav2521 <madhuri.y.0932@gmail.com>
2023-05-29 12:16:53 +0530

    updated

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,49 +1,6 @@
 import Users from "../modals/Users.js";
 
 
-
-export const login =  async(req, res) => {
-    try{
-        const{userEmail,userPassword} = req.body;
-        if(!userEmail) return res.send("User Email is required");
-        if(!userPassword) return res.send("User Password is required");
-        const response = await Users.find({email : userEmail}).exec();
-        if(response.length){
-            if(userPassword === response[0].password){
-                return res.send("you are logged in");
-            }else{
-                return res.send("Wrong password");
-            }
-        }else{
-            return res.send("User not found check your email..")
-        }
-    }catch(error){
-        return res.send(error)
-    }
-}
-
-export const updateUserName = async (req, res) => {
-    try {
-        const {email,name } = req.body;
-        if (!email) return res.send("Email not found!")
-        const response = await Users.find({email}).exec();
-        console.log(response);
-        if (response) {
-            const res = await Users.updateOne({email},{$set: {name : name }});
-            await res.save();
-            return res.send("record updated")
-            // return res.send(response[0])
-        } else {
-            return res.send("User not found!")
-        }
-    } catch (error) {
-        return res.send(error)
-    }
-}
-
-
-
-
 export const register = async (req, res) => {
     try {
         const { userName, userEmail, userPassword, userConfirmPassword } = req.body;
@@ -62,7 +19,7 @@ export const register = async (req, res) => {
         }
         const response = await Users.find({ email: userEmail }).exec();
         // console.log(response,"response")
-        if (response.length ) {
+        if (response.length) {
             return res.send("Email is already Taken or You are already resgistered!!");
         }
         const user = new Users({
@@ -72,6 +29,45 @@ export const register = async (req, res) => {
         });
         await user.save();
         return res.send("Resgistration Succesfull!")
+    } catch (error) {
+        return res.send(error)
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { userEmail, userPassword } = req.body;
+        if (!userEmail) return res.send("User Email is required");
+        if (!userPassword) return res.send("User Password is required");
+        const response = await Users.find({ email: userEmail }).exec();
+        if (response.length) {
+            if (userPassword === response[0].password) {
+                return res.send("you are logged in");
+            } else {
+                return res.send("Wrong password");
+            }
+        } else {
+            return res.send("User not found check your email..")
+        }
+    } catch (error) {
+        return res.send(error)
+    }
+}
+
+export const updateUserName = async (req, res) => {
+    try {
+        const { email, name } = req.body;
+        if (!email) return res.send("Email not found!")
+        const response = await Users.find({ email }).exec();
+        console.log(response);
+        if (response) {
+            const res = await Users.updateOne({ email }, { $set: { name: name } });
+            await res.save();
+            return res.send("record updated")
+            // return res.send(response[0])
+        } else {
+            return res.send("User not found!")
+        }
     } catch (error) {
         return res.send(error)
     }
